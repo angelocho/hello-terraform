b926b3c8ee500a8f1c63740d59a802c9ae0deafb
angelocho64 <angelocho64@gmail.com>
2023-02-14 18:58:50 +0100

    Annadido gitignore

--- main.tf
+++ main.tf
@@ -16,9 +16,13 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0b752bf1df193a6c4"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = [
+    "sg-0d2b1e710a2281426",
+  ]
+  subnet_id = "subnet-0f79bf7d4bc65f63b"
+  key_name = "clave-lucatic"  
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Terraforminstancia"
     APP  = "vue2048"
   }
 }

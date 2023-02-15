dee4ebc292081b2826784cd8f3b9f879fec1e722
angelocho64 <angelocho64@gmail.com>
2023-02-15 10:46:01 +0100

    user_data como file

--- main.tf
+++ main.tf
@@ -21,26 +21,7 @@ resource "aws_instance" "app_server" {
   ]
   subnet_id = "subnet-0f79bf7d4bc65f63b"
   key_name  = "clave-lucatic"
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install docker -y
-
-service docker start
-systemctl enable docker
-
-usermod -a -G docker ec2-user
-
-pip3 install docker-compose
-
-wget https://raw.githubusercontent.com/angelocho/hello-amazon/main/compose.yaml
-
-chown ec2-user compose.yaml
-
-docker-compose pull
-
-docker-compose up -d
-
-EOF
+  user_data = file("cloudinit.sh")
   tags = {
     Name = var.instance_name
     APP  = "vue2048"
